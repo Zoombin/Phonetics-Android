ca2e2834d2594fdc6c9815c6bce79d0f06854853
renwfy <renwfy@126.com>
2015-07-21 00:19:39 +0800

    完善首页； 加载数据

---
+++ phonetics/src/main/java/phonetics/android/adapter/PhoneticsAdapter.java
@@ -0,0 +1,79 @@
+package phonetics.android.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+
+import phonetics.android.R;
+import phonetics.android.entity.PhoneticsEntity;
+
+public class PhoneticsAdapter extends BaseAdapter {
+    List<PhoneticsEntity.SymbolEty> list;
+
+    public void addData(List<PhoneticsEntity.SymbolEty> list) {
+        if (this.list == null) {
+            this.list = new ArrayList<PhoneticsEntity.SymbolEty>();
+        }
+        this.list.addAll(list);
+        this.notifyDataSetChanged();
+    }
+
+    public void setData(List<PhoneticsEntity.SymbolEty> list) {
+        this.list = list;
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return list == null ? 0 : list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return list == null ? null : list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return list == null ? 0 : position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        ViewHodler viewHodler = null;
+        if (convertView == null) {
+            viewHodler = new ViewHodler();
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_phonetics_item, null);
+            viewHodler.iv_name = (TextView)convertView.findViewById(R.id.iv_name);
+            viewHodler.iv_description = (TextView)convertView.findViewById(R.id.iv_description);
+            viewHodler.recyclerview = (RecyclerView) convertView.findViewById(R.id.recyclerview);
+            convertView.setTag(viewHodler);
+        } else {
+            viewHodler = (ViewHodler) convertView.getTag();
+        }
+
+        PhoneticsEntity.SymbolEty ety = (PhoneticsEntity.SymbolEty) getItem(position);
+        if (ety != null) {
+            viewHodler.iv_name.setText(ety.getTitle());
+            viewHodler.iv_description.setText(ety.getDescribe());
+        }
+        return convertView;
+    }
+
+    class ViewHodler {
+        TextView iv_name;
+        TextView iv_description;
+        RecyclerView recyclerview;
+    }
+}
--- phonetics/src/main/java/phonetics/android/ui/MainActivity.java
+++ phonetics/src/main/java/phonetics/android/ui/MainActivity.java
@@ -2,25 +2,33 @@ package phonetics.android.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
 import phonetics.android.BaseActivity;
 import phonetics.android.R;
+import phonetics.android.adapter.PhoneticsAdapter;
 import phonetics.android.entity.CurrentPhonetics;
 import phonetics.android.entity.PhoneticsEntity;
 import phonetics.android.utils.AssetsUtil;
+import phonetics.android.widght.CustomDialog;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements OnClickListener {
+    TextView bt_left, bt_right;
+    ImageView iv_menu;
+    PhoneticsAdapter adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if(CurrentPhonetics.instance().entity == null){
-            CurrentPhonetics.instance().loadData(mActivity);
-        }
 
         initView();
     }
@@ -28,10 +36,87 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        if (CurrentPhonetics.instance().entity == null) {
+            CurrentPhonetics.instance().loadData(mActivity);
+        }
+
+        setData();
     }
 
-    private void initView(){
+    private void initView() {
+        (bt_left = (TextView) findViewById(R.id.bt_left)).setOnClickListener(this);
+        bt_left.setSelected(true);
 
+        (bt_right = (TextView) findViewById(R.id.bt_right)).setOnClickListener(this);
+        (iv_menu = (ImageView) findViewById(R.id.iv_menu)).setOnClickListener(this);
+        listView = (ListView)findViewById(R.id.listview);
+        listView.setAdapter(adapter = new PhoneticsAdapter());
     }
 
+    private void tabSelect(View v) {
+        bt_right.setSelected(false);
+        bt_left.setSelected(false);
+        v.setSelected(true);
+
+        PhoneticsEntity entity = CurrentPhonetics.instance().entity;
+        switch (v.getId()) {
+            case R.id.bt_left:
+                adapter.setData(entity.getBasics());
+                break;
+            case R.id.bt_right:
+                adapter.setData(entity.getAdvanced());
+                break;
+        }
+    }
+
+    private void showMenuDialog(){
+        final CustomDialog dialog = CustomDialog.create(mActivity,R.style.customDialogOne);
+        View view = LayoutInflater.from(mActivity).inflate(R.layout.layout_menu_dialog,null);
+        view.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        view.findViewById(R.id.tv_colse).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        view.findViewById(R.id.tv_item_compare).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        view.findViewById(R.id.tv_item_share).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        dialog.setContentVw(view);
+        dialog.show();
+    }
+
+    private void setData(){
+        PhoneticsEntity entity = CurrentPhonetics.instance().entity;
+        adapter.setData(entity.getBasics());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_left:
+                tabSelect(v);
+                break;
+            case R.id.bt_right:
+                tabSelect(v);
+                break;
+            case R.id.iv_menu:
+                showMenuDialog();
+                break;
+        }
+    }
 }
