0adf539471b2c7e9e27d5b275c49a72320997476
李少东 <renwfy@126.com>
2015-08-03 18:01:24 +0800

    新手引导

--- phonetics/src/main/java/phonetics/android/view/GuideDialog.java
+++ phonetics/src/main/java/phonetics/android/view/GuideDialog.java
@@ -14,6 +14,10 @@ import phonetics.android.widget.CustomDialog;
  * Created by lsd on 15/8/2.
  */
 public class GuideDialog {
+    /***
+     * 首页英标
+     * @param context
+     */
     public static void Step1(final Context context) {
         final CustomDialog dialog = CustomDialog.create(context, R.style.customDialogOne);
         View view = LayoutInflater.from(context).inflate(R.layout.layout_step1_dialog, null);
@@ -26,7 +30,7 @@ public class GuideDialog {
         view.findViewById(R.id.indicator_tap).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dialog.dismiss();
                 CurrentPhonetics.instance().setCurrentVoice(CurrentPhonetics.instance().basicsVoiceList.get(0));
                 context.startActivity(new Intent(context, DetailsActivity.class));
             }
@@ -35,6 +39,10 @@ public class GuideDialog {
         dialog.show();
     }
 
+    /***
+     * 简单介绍
+     * @param context
+     */
     public static void Step2(final Context context) {
         final CustomDialog dialog = CustomDialog.create(context, R.style.customDialogOne);
         View view = LayoutInflater.from(context).inflate(R.layout.layout_step2_dialog, null);
@@ -55,6 +63,10 @@ public class GuideDialog {
         dialog.show();
     }
 
+    /***
+     * 头像介绍
+     * @param context
+     */
     public static void Step3(final Context context) {
         final CustomDialog dialog = CustomDialog.create(context, R.style.customDialogOne);
         View view = LayoutInflater.from(context).inflate(R.layout.layout_step3_dialog, null);
@@ -67,7 +79,102 @@ public class GuideDialog {
         view.findViewById(R.id.tv_next).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dialog.dismiss();
+                Step4(context);
+            }
+        });
+        dialog.setContentVw(view);
+        dialog.show();
+    }
 
+    /***
+     * 点击举例
+     * @param context
+     */
+    public static void Step4(final Context context) {
+        final CustomDialog dialog = CustomDialog.create(context, R.style.customDialogOne);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_step4_dialog, null);
+        view.findViewById(R.id.tv_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        view.findViewById(R.id.indicator_tap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Step5(context);
+            }
+        });
+        dialog.setContentVw(view);
+        dialog.show();
+    }
+
+    /****
+     * 点击嘴型
+     * @param context
+     */
+    public static void Step5(final Context context) {
+        final CustomDialog dialog = CustomDialog.create(context, R.style.customDialogOne);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_step5_dialog, null);
+        view.findViewById(R.id.tv_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        view.findViewById(R.id.indicator_tap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Step6(context);
+            }
+        });
+        dialog.setContentVw(view);
+        dialog.show();
+    }
+
+    /***
+     * 点击正面/侧面
+     * @param context
+     */
+    public static void Step6(final Context context) {
+        final CustomDialog dialog = CustomDialog.create(context, R.style.customDialogOne);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_step6_dialog, null);
+        view.findViewById(R.id.tv_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        view.findViewById(R.id.indicator_tap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.setContentVw(view);
+        dialog.show();
+    }
+
+    /***
+     * 点击步骤发音
+     * @param context
+     */
+    public static void Step7(final Context context) {
+        final CustomDialog dialog = CustomDialog.create(context, R.style.customDialogOne);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_step6_dialog, null);
+        view.findViewById(R.id.tv_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        view.findViewById(R.id.indicator_tap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
             }
         });
         dialog.setContentVw(view);
